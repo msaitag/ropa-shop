bec4a3531f47d4d2a6244dd024caf47f71932747
msaitag <88306395+msaitag@users.noreply.github.com>
2024-07-12 18:04:50 +0200

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ const LoginSignup = () => {
   const login = async () =>{
     console.log("Login function Executed");
     let responseData;
-    await fetch ('http://localhost:4000/login',{
+    await fetch ('https://ropa-shop-backend.onrender.com/login',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
@@ -37,7 +37,7 @@ const LoginSignup = () => {
   const signup = async () =>{
     console.log("Sign Up Function Executed", formData);
     let responseData;
-    await fetch ('http://localhost:4000/signup',{
+    await fetch ('https://ropa-shop-backend.onrender.com/signup',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
