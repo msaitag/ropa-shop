f12d9630de318e5497cd370330055fee3e86236e
msaitag <88306395+msaitag@users.noreply.github.com>
2024-07-12 10:56:12 +0300

    first-commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis at nisi consequatur quidem itaque officia deserunt animi vitae assumenda. Optio laudantium quibusdam consequatur reprehenderit, similique ullam incidunt illum neque itaque, perspiciatis officia corrupti deserunt est totam natus culpa numquam exercitationem ratione ab omnis non maxime animi alias! Id, similique incidunt?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore doloremque atque reiciendis eligendi ut perferendis. Et eos ab sapiente, neque soluta est molestiae ducimus eligendi.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
