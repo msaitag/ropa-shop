a11fe9e2b5a62d5a63755cd0d0715c8e074130f7
msaitag <88306395+msaitag@users.noreply.github.com>
2024-07-13 13:33:21 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <div>
       <BrowserRouter>
       <Navbar/> 
-      <Routes basename="/tothepoint_login">
+      <Routes>
         <Route path='/' element={<Shop />} />
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
