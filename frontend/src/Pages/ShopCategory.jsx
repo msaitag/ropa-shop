377faf2dc8439de9e6653573f8be7c77a6e63ee3
msaitag <88306395+msaitag@users.noreply.github.com>
2024-07-15 20:45:27 +0200

    Update ShopCategory.jsx

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -7,7 +7,6 @@ import Item from '../Components/Item/Item'
 const ShopCategory = (props) => {
     const {all_product} = useContext(ShopContext);
   return (
-      <></>
     <div className='shop-category'>
       <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className='shopcategory-indexSort'>
